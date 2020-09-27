1b09a32d8190a1f0e538aae3a46ddd909d204614
ceciliakemiac <ceciliakemiac2@gmail.com>
2020-09-27 10:20:43 -0300

    Adicionando cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 8082;
 

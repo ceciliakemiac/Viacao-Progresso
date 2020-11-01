5310dd57073c040688a2626d2b0a296902958963
ceciliakemiac <ceciliakemiac2@gmail.com>
2020-11-01 14:42:36 -0300

    adding about page

--- frontend/src/pages/Corpo/AboutCorpo.js
+++ frontend/src/pages/Corpo/AboutCorpo.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import SlideShow from 'react-image-show';
 
+import styles from './AboutCorpo.module.css';
 import BaseService from '../../service/axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AboutCorpo = props => {
   const { id } = useParams();
@@ -16,24 +19,41 @@ const AboutCorpo = props => {
     nome: '',
     tipo: ''
   });
+  const [imagens, setImagens] = useState([]);
 
   useEffect(() => {
     let path = `/destinos/${id}`;
     BaseService.getServer(path)
       .then(res => {
         setCorpo({
-          ...corpo,
           ...res.data.data.destino,
-        })
+        });
+        setImagens([BaseService.baseUrl + `/imagens/${res.data.data.destino.image1}`,
+        BaseService.baseUrl + `/imagens/${res.data.data.destino.image2}`]);
       })
       .catch(error => console.log(error))
   }, [id]);
 
   return (
     <div >
-      {corpo.nome}
-      <br />
-      {corpo.descricao}
+      <h2 className={styles.nome} >{corpo.nome}</h2>
+      <div className={styles.main}>
+        <SlideShow
+          images={imagens}
+          width="700px"
+          imagesWidth="600px"
+          imagesHeight="550px"
+          imagesHeightMobile="56vw"
+          arrows={false}
+          fixedImagesHeight
+          indicators
+        />
+        <div className={styles.infos} >
+          {corpo.descricao}
+          {/* <FontAwesomeIcon icon={["fas", "coffee"]} /> */}
+          {/* not working ?? */}
+        </div>
+      </div>
     </div >
   );
 }

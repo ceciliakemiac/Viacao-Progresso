b114c3a86c22ae4508ab1b17427cce459aece414
ceciliakemiac <ceciliakemiac2@gmail.com>
2020-11-21 21:18:41 -0300

    adding checkbox

--- frontend/src/service/axios.js
+++ frontend/src/service/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getToken } from './auth';
 
 class BaseService {
   constructor() {
@@ -26,6 +27,18 @@ class BaseService {
     const response = await axios.post(url, body)
       .catch(error => console.log('[LOGIN ERROR] ' + error));
 
+    return response;
+  }
+
+  addUsuarioDestino = async (body) => {
+    const url = `${this.baseUrl}/usuarios/ondefui`;
+    console.log("TOKEN: ", getToken());
+    const response = await axios.post(url, body, {
+      headers: {
+        'x-auth-token': getToken(),
+      }
+    })
+      .catch(error => console.log('[ADD USUARIO DESTIONO ERROR] ' + error))
     return response;
   }
 
